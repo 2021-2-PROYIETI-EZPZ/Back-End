93cc90c4eefdeeecfd1ad72f0bb3258965e878cb
camilosaenz <48153830+camilosaenz@users.noreply.github.com>
2021-09-16 16:27:33 -0500

    Spring Initializr

---
+++ src/main/java/edu/eci/ezpz/EzpzApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ezpz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzpzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzpzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ezpz/EzpzApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ezpz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzpzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
