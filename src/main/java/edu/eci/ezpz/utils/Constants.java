83f07c8b43e2ae19f970875bf429273402258610
Alejandro Toro Daza <54339952+Skullzo@users.noreply.github.com>
2021-09-20 10:53:57 -0500

    Constants updated with new notify.

--- src/main/java/edu/eci/ezpz/utils/Constants.java
+++ src/main/java/edu/eci/ezpz/utils/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
 
     public static String[][] memberships = {
             { "MEM_01", "Notificaciones de vendedor", "El sistema notifica al usuario sobre las nuevas actualizaciones de sus vendedores favoritos" },
-            {"MEM_02", "Notificaciones de cliente", "El sistema notificara cuando un usuario realice busquedas en su pagina"}
+            { "MEM_02", "Notificaciones de cliente", "El sistema notificara cuando un usuario realice busquedas en su pagina"},
+            { "MEM_03", "Notificaciones de administrador", "El sistema notificara cuando el administrador realice cambios en cuanto a administracion de la pagina"}
     };
 
 
