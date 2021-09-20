6a46500d50cabc886b614dac4238ac01423165e0
Alejandro Toro Daza <54339952+Skullzo@users.noreply.github.com>
2021-09-20 09:24:54 -0500

    AdministratorService done.

--- src/main/java/edu/eci/ezpz/service/AdministratorService.java
+++ src/main/java/edu/eci/ezpz/service/AdministratorService.java
@@ -1,4 +1,7 @@
 package edu.eci.ezpz.service;
-
+import edu.eci.ezpz.controller.administrator.AdministratorDto;
+import edu.eci.ezpz.repository.document.Administrator;
 public interface AdministratorService {
+    public Administrator createAdministrator(AdministratorDto dto);
+    public boolean deleteAdministrator( String email );
 }
