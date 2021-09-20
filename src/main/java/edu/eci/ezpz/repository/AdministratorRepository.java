4877a2f5e4a88366d7aa1eb59bea4e5f247eada2
Alejandro Toro Daza <54339952+Skullzo@users.noreply.github.com>
2021-09-20 09:11:14 -0500

    Repository updated

--- src/main/java/edu/eci/ezpz/repository/AdministratorRepository.java
+++ src/main/java/edu/eci/ezpz/repository/AdministratorRepository.java
@@ -1,4 +1,11 @@
 package edu.eci.ezpz.repository;
 
-public interface AdministratorRepository {
+import edu.eci.ezpz.repository.document.Administrator;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AdministratorRepository extends MongoRepository<Administrator, String > {
+
+
+
+
 }
