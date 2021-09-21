17058985a6e074ac1e4da530c90d77bdfab90ef1
jfpazto <jfpazto@hotmail.com>
2021-09-21 18:36:48 +0100

    Create new collection in postman

--- src/main/java/edu/eci/ezpz/utils/Constants.java
+++ src/main/java/edu/eci/ezpz/utils/Constants.java
@@ -5,6 +5,4 @@ public class Constants {
     public static String[][] memberships = {
             { "MEM_01", "Notificaciones de vendedor", "El sistema notifica al usuario sobre las nuevas actualizaciones de sus vendedores favoritos" }
     };
-
-
 }
