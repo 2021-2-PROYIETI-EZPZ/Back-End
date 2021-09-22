2663505ff51e62d0360beea9896c9cfb8a590d02
Carlos A <54123771+Carlos96999@users.noreply.github.com>
2021-09-21 20:44:17 -0500

    Complemento

--- src/main/java/edu/eci/ezpz/config/SecurityConfiguration.java
+++ src/main/java/edu/eci/ezpz/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package edu.eci.ezpz.config;
 
+import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,23 +9,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
+    JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfiguration(@Autowired JwtRequestFilter jwtRequestFilter)
+    {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http)
             throws Exception
     {
-        http.cors().and().csrf().disable()
+        http.addFilterBefore( jwtRequestFilter, BasicAuthenticationFilter.class ).cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers( HttpMethod.GET, "/products" ).permitAll()
-                .antMatchers( HttpMethod.POST,"/register" ).permitAll()
-                .antMatchers( HttpMethod.POST,"/newProduct" ).hasRole("SELLER")
-                .antMatchers( HttpMethod.POST,"/newSeller" ).hasRole("ADMINISTRATOR")
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .antMatchers(HttpMethod.GET, "/v1/health").permitAll()
+                .antMatchers(HttpMethod.POST, "/v1/auth" ).permitAll()
+                .antMatchers(HttpMethod.POST,"/newProduct" ).hasRole("SELLER")
+                .antMatchers(HttpMethod.PUT,"/newProduct" ).hasRole("SELLER")
+                .antMatchers(HttpMethod.DELETE,"/newProduct" ).hasRole("SELLER")
+                .antMatchers(HttpMethod.POST,"/newSeller" ).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/newSeller" ).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/newSeller" ).hasRole("ADMIN")
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS );
     }
 }
