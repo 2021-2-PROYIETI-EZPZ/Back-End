21ef918ed831fa394d93368acc4924379e4be3d0
JoseGutierrezMarin <anesjoseguti@gmail.com>
2021-11-22 21:44:28 -0500

    Ilinio Done

--- src/main/java/edu/eci/ezpz/service/impl/CrawlerServiceImpl.java
+++ src/main/java/edu/eci/ezpz/service/impl/CrawlerServiceImpl.java
@@ -1,6 +1,7 @@
 package edu.eci.ezpz.service.impl;
 
 import edu.eci.ezpz.controller.crawler.ProductDto;
+import edu.eci.ezpz.repository.document.Product;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,6 +9,7 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -20,34 +22,14 @@ public class CrawlerServiceImpl implements edu.eci.ezpz.service.CrawlerService {
     public ArrayList<ProductDto> startWebCrawling(String requestedProduct) {
         ArrayList<ProductDto> products = new ArrayList<>();
         //Mercado libre
-        String query = requestedProduct.replace(" ", "-");
-        System.out.println("El query: "+query);
-        try {
-            Document doc =  Jsoup.connect("https://listado.mercadolibre.com.co/"+query).get();
-            Elements cards =  doc.select("div.andes-card.andes-card--flat.andes-card--default.ui-search-result.ui-search-result--core.andes-card--padding-default");
-            ProductDto product = new ProductDto();
-            for (Element e : cards){
-                URL url = new URL(e.select("div.slick-slide.slick-active").get(0).child(0).attr("data-src"));
-                String encodedImg = getEncodedImage( url );
-                String itemUrl = e.select("a.ui-search-link").attr("href");
-                String price = e.select("span.price-tag-fraction").get(0).ownText();
-                String title = e.select("h2.ui-search-item__title").get(0).ownText();
-                String site = "Mercado Libre";
-                product.setTitle(title);
-                product.setPrice(price);
-                product.setSite(site);
-                product.setImg(encodedImg);
-                product.setUrl(itemUrl);
-                products.add(product);
-            }
+        products = crawlMercadoLibre(products, requestedProduct);
 
+        //Ilinio
+        products = crawlIlinio( products, requestedProduct );
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    return products;
+        return products;
     }
+
 
     private String getEncodedImage( URL url ){
         InputStream in = null;
@@ -69,6 +51,60 @@ public class CrawlerServiceImpl implements edu.eci.ezpz.service.CrawlerService {
             e.printStackTrace();
         }
         return encodedImg;
+    }
+
+
+    private ArrayList<ProductDto> crawlMercadoLibre(ArrayList<ProductDto> products, String requestedProduct){
+        String query = requestedProduct.replace(" ", "-");
+        try {
+            Document doc =  Jsoup.connect("https://listado.mercadolibre.com.co/"+query).get();
+            Elements cards =  doc.select("div.andes-card.andes-card--flat.andes-card--default.ui-search-result.ui-search-result--core.andes-card--padding-default");
+            ProductDto product = new ProductDto();
+            for (Element e : cards){
+                URL url = new URL(e.select("div.slick-slide.slick-active").get(0).child(0).attr("data-src"));
+                String encodedImg = getEncodedImage( url );
+                String itemUrl = e.select("a.ui-search-link").attr("href");
+                String price = e.select("span.price-tag-fraction").get(0).ownText();
+                String title = e.select("h2.ui-search-item__title").get(0).ownText();
+                String site = "Mercado Libre";
+                product.setTitle(title);
+                product.setPrice(price);
+                product.setSite(site);
+                product.setImg(encodedImg);
+                product.setUrl(itemUrl);
+                products.add(product);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    private ArrayList<ProductDto> crawlIlinio( ArrayList<ProductDto> products, String requestedProduct ){
+        String query = requestedProduct.replace(" ", "+");
+        try {
+            Document doc =  Jsoup.connect("https://www.linio.com.co/search?scroll=&q="+requestedProduct).get();
+            Elements cards = doc.select("div.catalogue-product.row");
+
+            ProductDto product = new ProductDto();
+            for (Element e : cards){
+                URL url = new URL("https:"+e.select("img.image").attr("data-lazy"));
+                String encodedImg = getEncodedImage( url );
+                String itemUrl = "https://www.linio.com.co"+e.select("a.col-12.pl-0.pr-0").attr("href");
+                String price = e.select("span.price-main-md").get(0).ownText();
+                String title = e.select("span.title-section").get(0).ownText();
+                String site = "AliExpress";
+                product.setTitle(title);
+                product.setPrice(price);
+                product.setSite(site);
+                product.setImg(encodedImg);
+                product.setUrl(itemUrl);
+                products.add(product);
+           }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
 
